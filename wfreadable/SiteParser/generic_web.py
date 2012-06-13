081384e5d389e1c3441bd837fc80446a91deb25d
syshen <sysheen@gmail.com>
2012-06-13 17:45:14 +0800

    remove redundant code

--- wfreadable/SiteParser/generic_web.py
+++ wfreadable/SiteParser/generic_web.py
@@ -4,39 +4,6 @@ import os
 import urlparse
 import re
 
-def fix_src(url, html_tree, url_base=None):
-    uri_path = ""
-    if url_base is None:
-        urlparts = urlparse.urlparse(url)
-        if (urlparts.scheme == ''):
-            url_base = 'http://' + urlparts.netloc 
-        else:
-            url_base = urlparts.scheme + '://' + urlparts.netloc
-
-        uri_path = os.path.dirname(urlparts.path)
-
-    regex = "(http|https|ftp)://.*"
-    pattern = re.compile(regex)
-
-    imgs = html_tree.xpath("//img")
-    for img in imgs: 
-        src = img.get("src")
-        if src is not None and not pattern.match(src):
-            if src[0] == '/':
-                img.set("src", "{0}{1}".format(url_base, src))
-            else:
-                img.set("src", "{0}{1}{2}".format(url_base, uri_path, src))
-
-    atags = html_tree.xpath("//a")
-    for atag in atags:
-        href = atag.get("href")
-        if href is not None and not pattern.match(href):
-            if href[0] == '/':
-                atag.set("href", "{0}{1}".format(url_base, href))
-            else:
-                atag.set("href", "{0}{1}{2}".format(url_base, uri_path, href))
-
-
 class GenericWeb(object):
     def __init__(self, verbose=False):
         self.verbose = verbose
