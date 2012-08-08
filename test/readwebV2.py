b0e8674a9178a5646bf72e4366b82b277f363181
Kudo Chien <ckchien@gmail.com>
2012-08-08 16:14:01 +0800

    Add v2 test script

---
+++ test/readwebV2.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import json
+sys.path.append((os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+from wfreadable.WFReadable import *
+
+url = sys.argv[1]
+
+#try:
+parser = WFReadableV2(url)
+obj = parser.parse()
+print json.dumps(obj, indent=4)
+#except PageFetchError:
+#    print "Fail to fetch web page"
+#except WebParseError:
+#    print "Fail to parse web structure"
+#except WebSummarizeError:
+#    print "Fail to summarize web content"
